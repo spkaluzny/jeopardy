90b0e4eb07a57fb1633e436ec7d7665dd0995282
spkal <spk@seapowet.org>
2015-04-20 09:42:23 -0700

    Updated genderScript.R to match gender for unique first names back to all names.

--- genderScript.R
+++ genderScript.R
@@ -9,4 +9,8 @@ firstName <- with(dfJeop03, gsub("^(.*?)\\s.*", "\\1", Name1))
 # Estimate gender using the gender from the gender package
 # Only do the unique firstNames
 uFirstName <- unique(firstName)
+# This takes 237.73 secs. on boulder:
 uGenderList <- gender(uFirstName)
+uGender <- sapply(uGenderList, function(x) x$gender)
+uPropMale <- sapply(uGenderList, function(x) x$proportion_male)
+mFirstName <- match(firstName, uFirstName)
