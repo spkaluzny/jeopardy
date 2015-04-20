ba8329c721f6fe4b68b4c57f18614a98dcd1c06a
spkal <spk@seapowet.org>
2015-04-20 08:38:16 -0700

    Update to genderScript.R.

--- genderScript.R
+++ genderScript.R
@@ -6,8 +6,7 @@ dfJeop03 <- select(dfJeop02, Name, PlayerID) %>%
     summarise(Name1 = first(Name))
 # Extract first name:
 firstName <- with(dfJeop03, gsub("^(.*?)\\s.*", "\\1", Name1))
-# Estimate gender using the gender from the gedner package
-# 
-Gender <- sapply(firstName, function(x) gender(x)$gender)
-
-
+# Estimate gender using the gender from the gender package
+# Only do the unique firstNames
+uFirstName <- unique(firstName)
+uGenderList <- gender(uFirstName)
