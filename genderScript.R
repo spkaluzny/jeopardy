62ca51a75b947b5389956f54cd61c59dfdd72e47
Stephen Kaluzny <spk@seadevl6401.(none)>
2015-04-20 23:08:11 -0700

    Added show gender tryad computation.

--- genderScript.R
+++ genderScript.R
@@ -1,6 +1,5 @@
 require(dplyr)
 require(gender)
-require(ggplot2)
 firstName <- with(dfJeop02, gsub("^(.*?)\\s.*", "\\1", Name))
 # Estimate gender using the gender from the gender package
 # Only do the unique firstNames
@@ -8,18 +7,31 @@ uFirstName <- unique(firstName)
 # This takes 232.14 secs on boulder:
 uGenderList <- gender(uFirstName)
 uGender <- sapply(uGenderList, function(x) x$gender)
+# Code "male" -> "M", "female" -> "F"
+uGender <- ifelse(uGender == "male", "M", uGender)
+uGender <- ifelse(uGender == "female", "F", uGender)
 uPropMale <- sapply(uGenderList, function(x) x$proportion_male)
 uPropFemale <- sapply(uGenderList, function(x) x$proportion_female)
 # Only consider gender that has prop. greater than propCutoff,
 # otherwise, declare it as NA
 propCutoff <- 0.75
-uGender <- ifelse((!is.na(uGender) & uGender == "male" &
+uGender <- ifelse((!is.na(uGender) & uGender == "M" &
     uPropMale < propCutoff), NA, uGender)
-uGender <- ifelse((!is.na(uGender) & uGender == "female" &
+uGender <- ifelse((!is.na(uGender) & uGender == "F" &
     uPropFemale < propCutoff), NA, uGender)
 mFirstName <- match(firstName, uFirstName)
 dfJeop02$Gender <- uGender[mFirstName]
+#
+# Show gender tryad:
+trigen <- function(gen) {
+    if(any(is.na(gen))) {
+        "na"
+    } else {
+        paste0(sort(gen), collapse = "")
+    }
+}
 dfJeop02 %>%
-    select(Name, Gender) %>%
-    sample_n(30, replace = FALSE)
-
+    group_by(Show) %>%
+    summarise(ShowGender = trigen(Gender)) %>%
+    with(table(ShowGender))
+    
