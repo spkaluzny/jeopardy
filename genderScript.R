0a307f9b16b014c7c3a4a933d172031f535e84d9
Stephen Kaluzny <spk@seadevl6401.(none)>
2015-04-20 19:10:14 -0700

    Update to gender compuations.

--- genderScript.R
+++ genderScript.R
@@ -1,16 +1,25 @@
 require(dplyr)
 require(gender)
 require(ggplot2)
-dfJeop03 <- select(dfJeop02, Name, PlayerID) %>%
-    group_by(PlayerID) %>%
-    summarise(Name1 = first(Name))
-# Extract first name:
-firstName <- with(dfJeop03, gsub("^(.*?)\\s.*", "\\1", Name1))
+firstName <- with(dfJeop02, gsub("^(.*?)\\s.*", "\\1", Name))
 # Estimate gender using the gender from the gender package
 # Only do the unique firstNames
 uFirstName <- unique(firstName)
-# This takes 237.73 secs. on boulder:
+# This takes 232.14 secs on boulder:
 uGenderList <- gender(uFirstName)
 uGender <- sapply(uGenderList, function(x) x$gender)
 uPropMale <- sapply(uGenderList, function(x) x$proportion_male)
+uPropFemale <- sapply(uGenderList, function(x) x$proportion_female)
+# Only consider gender that has prop. greater than propCutoff,
+# otherwise, declare it as NA
+propCutoff <- 0.75
+uGender <- ifelse((!is.na(uGender) & uGender == "male" &
+    uPropMale < propCutoff), NA, uGender)
+uGender <- ifelse((!is.na(uGender) & uGender == "female" &
+    uPropFemale < propCutoff), NA, uGender)
 mFirstName <- match(firstName, uFirstName)
+dfJeop02$Gender <- uGender[mFirstName]
+dfJeop02 %>%
+    select(Name, Gender) %>%
+    sample_n(30, replace = FALSE)
+
